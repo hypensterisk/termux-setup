d38342a1df30056915a52018967a3f221eeb2076
Roykhan <roykhanalaushaf0@gmail.com>
2024-03-17 15:10:47 +0700

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -7,6 +7,8 @@ echo "$username:$password" | chpasswd
 apk add sudo
 echo "$username ALL=(ALL:ALL) ALL" >> /etc/sudoers
 echo "Defaults:$username !authenticate" >> /etc/sudoers
+curl -o "/home/$username/.profile" https://raw.githubusercontent.com/hypensterisk/termux-setup/main/user.sh
+login -f $username
 echo "login -f $username" > ~/.profile
 echo 'read -p "Do you want to exit Termux? [Y/n]: " exit && [ "${exit}" == "n" ] || [ "${exit}" == "N" ] && exit 1 || exit 0' >> ~/.profile
 exit
