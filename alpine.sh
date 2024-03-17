0c2aefbe13c913d90592b8f48ff079889af3f5cc
Roykhan <roykhanalaushaf0@gmail.com>
2024-03-17 14:18:21 +0700

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -7,6 +7,6 @@ echo "$username:$password" | chpasswd
 apk add sudo
 echo "$username ALL=(ALL:ALL) ALL" >> /etc/sudoers
 echo "Defaults:$username !authenticate" >> /etc/sudoers
-echo "login -f $username" > .profile
-echo 'read -p "Do you want to exit Termux? [Y/n]: " exit && [ "${exit}" == "n" ] || [ "${exit}" == "N" ] && exit 1 || exit 0' >> .profile
+echo "login -f $username" > ~/.profile
+echo 'read -p "Do you want to exit Termux? [Y/n]: " exit && [ "${exit}" == "n" ] || [ "${exit}" == "N" ] && exit 1 || exit 0' >> ~/.profile
 exit
