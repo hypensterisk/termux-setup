2ad21e4ce87867408f212cd425ba6ebe7db4e058
Roykhan <roykhanalaushaf0@gmail.com>
2024-03-17 08:30:41 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@ termux-change-repo
 pkg upgrade
 pkg install proot-distro
 proot-distro install alpine
+ALPINE=var/lib/proot-distro/installed-rootfs/alpine
+curl -o "$PREFIX/$ALPINE/root/.profile" https://raw.githubusercontent.com/hypensterisk/termux-setup/main/alpine.sh
+proot-distro login alpine
