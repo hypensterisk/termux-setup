51ef76cddb62e3b3d99f1105ecbfe21df8165c63
Roykhan <roykhanalaushaf0@gmail.com>
2024-03-17 15:07:47 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,7 @@ proot-distro install alpine
 ALPINE=var/lib/proot-distro/installed-rootfs/alpine
 curl -o "$PREFIX/$ALPINE/root/.profile" https://raw.githubusercontent.com/hypensterisk/termux-setup/main/alpine.sh
 proot-distro login alpine
-echo "clear && proot-distro login alpine && exit" > ~/.profile
+echo "clear && proot-distro login alpine && exit" > $HOME/.profile
+curl -o "$HOME/.termux/font.ttf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/FiraCode/Regular/FiraCodeNerdFont-Regular.ttf
+pkg install tur-repo
+pkg install code-server
