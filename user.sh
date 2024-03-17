de9e3e507b4e1ee27fc7e942ada19ed514cc4a2f
Roykhan <roykhanalaushaf0@gmail.com>
2024-03-17 15:04:35 +0700

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,1 +1,6 @@
-
+curl -s https://ohmyposh.dev/install.sh | bash -s
+sudo apk add shadow bash
+username=$(whoami)
+sudo chsh -s /bin/bash $username
+echo 'eval "$(oh-my-posh init bash)"' > ~/.profile
+curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | bash
